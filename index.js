c9bff233e12d15ef9575fd89d2d2c2c028976ccc
Bhaminishivram <49640039+Bhaminishivram@users.noreply.github.com>
2021-06-16 16:51:58 +0530

    redux_project_process_explain_through_project

---
+++ index.js
@@ -0,0 +1,73 @@
+const {createStore, combineReducers} = require("redux") 
+//type
+const BUY_LAPTOP = "BUY_LAPTOP"; 
+const BUY_MOBILE = "BUY-MOBILE";
+//initialState 
+const initialState = {
+    numOfLaptops: 100
+}
+const initialMobiles ={
+    numOfMobiles :1000
+}
+
+//Actions
+const buyLaptop = () => {
+    return {
+        type: BUY_LAPTOP
+    }
+}   
+const buyMobile= () => {
+    return {
+        type: BUY_MOBILE
+    }
+}  
+
+//Reducers
+const laptopReducer = (state = initialState, action) => {
+    //   if(action.type ==="BUY_LAPTOP"){
+    //       return {numOfLaptops:state.numOfLaptops}
+    //   }else{
+    //       return state; 
+    //   } 
+    switch (action.type) {
+        case BUY_LAPTOP:
+            return { numOfLaptops: state.numOfLaptops - 1 }
+        default:
+            return state;
+    }
+
+ }  
+ //more than one reducers 
+
+ const mobileReducer = (state = initialMobiles, action) => {
+    //   if(action.type ==="BUY_LAPTOP"){
+    //       return {numOfLaptops:state.numOfLaptops}
+    //   }else{
+    //       return state; 
+    //   } 
+    switch (action.type) {
+        case BUY_MOBILE:
+            return { numOfMobiles: state.numOfMobiles - 1 }
+        default:
+            return state;
+    }
+
+ }  
+ //Store 
+ const rootReducer = combineReducers({laptop:laptopReducer,mobiles:mobileReducer})
+ const store = createStore(rootReducer);
+ //console.log(store); 
+ store.subscribe(()=> {console.log(store.getState())});
+ store.dispatch(buyLaptop());
+ store.dispatch(buyLaptop());
+ store.dispatch(buyLaptop()); 
+ store.dispatch(buyLaptop());
+ store.dispatch(buyLaptop());
+ store.dispatch(buyMobile());
+ store.dispatch(buyMobile());
+ store.dispatch(buyMobile());
+ store.dispatch(buyMobile());
+ store.dispatch(buyMobile());
+ store.dispatch(buyMobile());
+ store.dispatch(buyMobile());
+ store.dispatch(buyMobile());
